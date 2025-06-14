bf12caa6cb87777ae094fe3d68dbb6d0ff58fa3f
AishwaryaB149 <23951A0406@iare.ac.in>
2025-06-14 22:57:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.java
@@ -0,0 +1,24 @@
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+*/
+class Solution {
+    boolean isSym(Node a, Node b){
+        if(a==null||b==null) return a==b;
+        if(a.data!=b.data) return false;
+        return isSym(a.left,b.right)&&isSym(a.right,b.left);
+    }
+    public boolean isSymmetric(Node root) {
+        return root==null||isSym(root.left,root.right);
+        
+    }
+}
